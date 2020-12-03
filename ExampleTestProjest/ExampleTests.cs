04d4cd2a5ed391500f59d6e2b06bb4d58bc83241
Joe Thompson <joseph.thompson>
2020-12-03 14:17:06 +0000

    updated to use 3.1 instead of 2.0

--- ExampleTestProjest/ExampleTests.cs
+++ ExampleTestProjest/ExampleTests.cs
@@ -13,28 +13,7 @@ namespace ExampleTestProjest
         [TestMethod]
         public void Example()
         {
-            var execution = new Execution()
-                .ExceptionOnAssertionFailure(false)
-                .RetryCount(3, TimeSpan.FromSeconds(2))
-                .NavigateTo(new Uri("https://localhost"))
-                .Expect
-                    .ToBeOn(new Uri("https://localhost"))
-                    .ToBeAbleToSeeElement(Locator.From(By.ClassName("login")))
-                    .ToBeAbleToClickElement(Locator.From(By.ClassName("login")))
-                .Then
-                .Click(Locator.From(By.ClassName("login")))
-                .Wait
-                    .ForElementToBeDisabled(Locator.From(By.ClassName("login")), TimeSpan.FromSeconds(3))
-                    .ForElementToBeHidden(Locator.From(By.ClassName("login")), TimeSpan.FromSeconds(2))
-                .Then
-                    .NavigateTo(new Uri("https://localhost"))
-                .Utils
-                    .SetWindowMaximised() 
-                .Then
-                .Complete();
-
-            var factory = new ManagedWebDriverFactory(Browser.Chrome);
-            var result = execution.Execute(factory);
+           
         }
     }
 }
